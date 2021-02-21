0ba2f10683f727582513d8056ec86b92fa770cc6
mxb1137 <mxb1137@git-teaching.cs.bham.ac.uk>
2021-02-21 13:35:52 +0000

    Reorganised files, fixed some package declarations

--- src/game/ExampleFloor.java
+++ src/game/ExampleFloor.java
@@ -1,42 +1,33 @@
-package server;
 import java.awt.*;
-import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 import java.util.LinkedList;
 
 public class ExampleFloor extends GameObject implements GraphicalObject, SolidCollider{
+private static BufferedImage sprite;
+private static String url = "./img/Grass2.png";
+public ExampleFloor(float x, float y, int z) {
+    super(x, y, z, 640, 32);
+    sprite = this.loadImage(ExampleFloor.url);
+    CollidingObject.addCollider(this);
+    SolidCollider.addSolidCollider(this);
+    Game.handler.addObject(this);
+}
 
-	static BufferedImage sprite;
-	static String url = "./img/Cursor.png";
-	
-	public ExampleMouseListener() {
-		super(0, 100, 10, 32, 32);
-		sprite = this.loadImage(ExampleMouseListener.url);
-		CollidingObject.addCollider(this);	
-		SolidCollider.addCollider(this); 
-		Game.handler.addObject(this);
-	}
-	
-	
-	public void tick(double delta) {
-		CollidingObject.getCollisions(this);
-		
-	}
+public void tick(double delta) {
+    CollidingObject.getCollisions(this);
+    
+}
 
-	public void render(Graphics g) {
-		this.drawSprite(g, sprite, (int)this.x, (int)this.y);
-	}
+public void render(Graphics g) {
+    this.drawSprite(g, sprite, (int)this.x, (int)this.y);
+}
 
+@Override
+public void handleCollisions(LinkedList<CollidingObject> collisions) {
+}
 
-	@Override
-	public void handleCollisions(LinkedList<CollidingObject> collisions) {
-	}
-
-	@Override
-	public Rectangle getBounds() {
-		return new Rectangle((int)this.x, (int)this.y, this.width, this.height);
-	}
-
-	
-
+@Override
+public Rectangle getBounds() {
+    return new Rectangle((int)this.x, (int)this.y, this.width, this.height);
+}
 }
