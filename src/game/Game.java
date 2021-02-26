00f3cc71e54991384d0a49ee4a20dcb612784208
Sebastian Gabriel Savu <sgs020@student.bham.ac.uk>
2021-02-26 23:06:25 +0000

    Resolved a little map conflict

--- src/game/Game.java
+++ src/game/Game.java
@@ -4,39 +4,18 @@ import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Random;
-import java.util.Scanner;
-import javax.imageio.ImageIO;
 import game.display.Window;
-import game.entities.Platform;
 import game.entities.Player;
 import game.input.KeyInput;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
 
 public class Game extends Canvas implements Runnable{
 
 	private static final long serialVersionUID = -772676358550096683L;
 	private Thread thread;
 	private boolean running = false;
-
 	public static KeyInput keyInput = new KeyInput();
 	public static MouseInput mouseInput = new MouseInput();
-
-
-	private int horizontalIndex = 0;
-	private int verticalIndex = 0;
-	private int setX = 426;
-	private int setY = 384;
-	private String lastDirection="";
-	private String currentTheme="A";
-
-
-	
 	public static GameState state = GameState.MainMenu;
-
 	//	private LinkedList<Level> levels = new LinkedList<>();
 	private Level currentLevel = new Level();
 
@@ -48,16 +27,17 @@ public class Game extends Canvas implements Runnable{
 	 * Initialises game entities and objects that must appear at the start of the game
 	 */
 	public Game() {
+
+		player = new Player(0, 340,0,0,"./img/adventurer-idle-00.png", "./img/adventurer-idle-01.png", "./img/adventurer-idle-02.png");
+		currentLevel.addEntity(player);
+
 		//make this as a player choice in the menu either MAP 1 or Randomly Generated
 		String mapMode = "default";
-
+		Map m = new Map();
 		//keep default for now until we sort randomly generated
 		if (mapMode.equals("default")) {
-			mapParser(currentLevel, "./src/game/segmentA1.txt");
-			mapParser(currentLevel, "./src/game/segmentA2.txt");
-			mapParser(currentLevel, "./src/game/intersegmentA1.txt");
-			mapParser(currentLevel, "./src/game/intersegmentA2.txt");
-			mapParser(currentLevel, "./src/game/intersegmentA2.txt");
+
+			m.mapParser(currentLevel, "./src/game/intersegmentA2.txt");
 
 		}	//WORK IN PROGRESS
 		else if (mapMode.equals("randomlyGenerated"))
@@ -65,8 +45,6 @@ public class Game extends Canvas implements Runnable{
 
 		}
 
-		player = new Player(0, 340,0,0,"./img/adventurer-idle-00.png", "./img/adventurer-idle-01.png", "./img/adventurer-idle-02.png");
-		currentLevel.addEntity(player);
 
 		camera = new Camera();
 		camera.addTarget(player);
@@ -147,115 +125,7 @@ public class Game extends Canvas implements Runnable{
 		stop();
 	}
 
-	public void mapParser(Level currentLevel, String url)
-	{
-		Chunk c;
-		String goUrl ="";
-		String texturePlatformDefault="";
-		String texturePlatformInverted="";
-		String textureFloor ="";
-		String textureBackground="";
 
-		try {
-			File myObj = new File(url);
-			Scanner myReader = new Scanner(myObj);
-			while (myReader.hasNextLine()) {
-
-				String data = myReader.nextLine();
-				String[] splited = data.split("\\s+");
-
-				switch (splited[0]){
-
-					case "Theme":
-						if (!splited[1].equals(currentTheme))
-						{
-							setX = Integer.parseInt(splited[2]);
-							setY = Integer.parseInt(splited[3]);
-							currentTheme = splited[1];
-						}
-						texturePlatformDefault = splited[4];
-						texturePlatformInverted = splited[5];
-						textureFloor = splited[6];
-						textureBackground = splited[7];
-						break;
-
-					case "Chunk":
-						lastDirection= splited[2];
-						switch (splited[2]){
-							case "E":
-								horizontalIndex= horizontalIndex + setX;
-								break;
-							case "W":
-								horizontalIndex= horizontalIndex - setX;
-								break;
-							case "N":
-								verticalIndex = verticalIndex - setY;
-								break;
-							case "S":
-								verticalIndex = verticalIndex + setY;
-								break;
-						}
-						c = new Chunk(horizontalIndex-setX,verticalIndex,setX,setY,splited[3]);
-						currentLevel.addChunk(c);
-
-						break;
-
-					case "Platform":
-						Platform p;
-
-						switch (splited[1]){
-							case "Default":
-								goUrl = texturePlatformDefault;
-								break;
-							case "Inverted":
-								goUrl = texturePlatformInverted;
-								break;
-							case "Custom":
-								goUrl = splited[4];
-								break;
-						}
-
-						p = new Platform(horizontalIndex-setX+ Integer.parseInt(splited[2]), verticalIndex + Integer.parseInt(splited[3]) , 0, 0, goUrl);
-						currentLevel.addPlatform(p);
-						break;
-
-					case "Floor":
-						goUrl=textureFloor;
-						p = new Platform(horizontalIndex-setX , verticalIndex +setY , setX, 0, goUrl);
-						currentLevel.addPlatform(p);
-						break;
-					/*
-					case "Moving":
-						MovingPlatform xd = new MovingPlatform(horizontalIndex + Integer.parseInt(splited[2]), verticalIndex + Integer.parseInt(splited[3]) , 32,16,20,true,1	,"./img/platformA.png");
-						currentLevel.addPlatform(xd);
-					*/
-					case "Revert":
-						switch (lastDirection)
-						{
-							case "E":
-								horizontalIndex= horizontalIndex - setX;
-								break;
-							case "W":
-								horizontalIndex= horizontalIndex + setX;
-								break;
-							case "N":
-								verticalIndex = verticalIndex + setY;
-								break;
-							case "S":
-								verticalIndex = verticalIndex - setY;
-								break;
-
-						}
-						break;
-				}
-				System.out.println(data);
-			}
-			myReader.close();
-		} catch (FileNotFoundException e) {
-			System.out.println("An error occurred.");
-			e.printStackTrace();
-		}
-	}
 
 	public synchronized void start() {
 		//The server is started in this function
--- src/game/Map.java
+++ src/game/Map.java
@@ -1,86 +1,126 @@
 package game;
 
+import game.entities.Platform;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-import game.entities.Platform;
-
 public class Map {
-	
-	static int horizontalIndex = 0;
-	static int verticalIndex = 0;
-	static int newOffsetX = 0;
-	static int newOffsetY = 0;
-	static int t = 0;
-	
-	/**
-	 * WHAT DOES IT DO?
-	 * @param masterOffsetX
-	 * @param masterOffsetY
-	 * @param currentLevel
-	 * @param url
-	 */
-	public static void mapParser(int masterOffsetX, int masterOffsetY, Level currentLevel, String url)
-	{
 
-		Platform p;
-		Chunk c = new Chunk();
 
-		try {
-			File myObj = new File(url); //
-			Scanner myReader = new Scanner(myObj);
-			while (myReader.hasNextLine()) {
+    private int horizontalIndex = 0;
+    private int verticalIndex = 0;
+    private int setX = 426;
+    private int setY = 384;
+    private String lastDirection = "";
+    private String currentTheme = "A";
 
-				String data = myReader.nextLine();
-				String[] splited = data.split("\\s+");
 
-				switch (splited[0]){
-					case "Theme":
-						newOffsetX = Integer.parseInt(splited[2]);
-						newOffsetY = Integer.parseInt(splited[3]);
-						t = 1;
-						break;
-					case "Chunk":
-						currentLevel.addChunk(c);
-						c = new Chunk();
-						if (t==0) {
-							masterOffsetX = newOffsetX;
-							masterOffsetY = newOffsetY;
-						}
-						switch (splited[2]){
-							case "E":
-								horizontalIndex= horizontalIndex + masterOffsetX;
-								break;
-							case "W":
-								horizontalIndex= horizontalIndex - masterOffsetX;
-								break;
-							case "N":
-								verticalIndex = verticalIndex - masterOffsetY;
-								break;
-							case "S":
-								verticalIndex = verticalIndex + masterOffsetY;
-								break;
-						}
-						t--;
-						break;
-					case "Platform":
-						System.out.println(masterOffsetX);
-						System.out.println(newOffsetX);
-						System.out.println(horizontalIndex);
-						p = new Platform(horizontalIndex + Integer.parseInt(splited[1]), verticalIndex + Integer.parseInt(splited[2]) , Integer.parseInt(splited[3]), Integer.parseInt(splited[4]));
-						c.addPlatform(p);
-						break;
-				}
-				System.out.println(data);
+    public void mapParser(Level currentLevel, String url) {
+        Chunk c;
+        String goUrl = "";
+        String texturePlatformDefault = "";
+        String texturePlatformInverted = "";
+        String textureFloor = "";
+        String textureBackground = "";
 
-			}
-			currentLevel.addChunk(c);
-			myReader.close();
-		} catch (FileNotFoundException e) {
-			System.out.println("An error occurred.");
-			e.printStackTrace();
-		}
-	}
+        try {
+            File myObj = new File(url);
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
 
+                String data = myReader.nextLine();
+                String[] splited = data.split("\\s+");
+
+                switch (splited[0]) {
+
+                    case "Theme":
+                        if (!splited[1].equals(currentTheme)) {
+                            setX = Integer.parseInt(splited[2]);
+                            setY = Integer.parseInt(splited[3]);
+                            currentTheme = splited[1];
+                        }
+                        texturePlatformDefault = splited[4];
+                        texturePlatformInverted = splited[5];
+                        textureFloor = splited[6];
+                        textureBackground = splited[7];
+                        break;
+
+                    case "Chunk":
+                        lastDirection = splited[2];
+                        switch (splited[2]) {
+                            case "E":
+                                horizontalIndex = horizontalIndex + setX;
+                                break;
+                            case "W":
+                                horizontalIndex = horizontalIndex - setX;
+                                break;
+                            case "N":
+                                verticalIndex = verticalIndex - setY;
+                                break;
+                            case "S":
+                                verticalIndex = verticalIndex + setY;
+                                break;
+                        }
+                        c = new Chunk(horizontalIndex - setX, verticalIndex, setX, setY, splited[3]);
+                        currentLevel.addChunk(c);
+
+                        break;
+
+                    case "Platform":
+                        Platform p;
+
+                        switch (splited[1]) {
+                            case "Default":
+                                goUrl = texturePlatformDefault;
+                                break;
+                            case "Inverted":
+                                goUrl = texturePlatformInverted;
+                                break;
+                            case "Custom":
+                                goUrl = splited[4];
+                                break;
+                        }
+
+                        p = new Platform(horizontalIndex - setX + Integer.parseInt(splited[2]), verticalIndex + Integer.parseInt(splited[3]), 0, 0, goUrl);
+                        currentLevel.addPlatform(p);
+                        break;
+
+                    case "Floor":
+                        goUrl = textureFloor;
+                        p = new Platform(horizontalIndex - setX, verticalIndex + setY, setX, 0, goUrl);
+                        currentLevel.addPlatform(p);
+                        break;
+					/*
+					case "Moving":
+						MovingPlatform xd = new MovingPlatform(horizontalIndex + Integer.parseInt(splited[2]), verticalIndex + Integer.parseInt(splited[3]) , 32,16,20,true,1	,"./img/platformA.png");
+						currentLevel.addPlatform(xd);
+					*/
+                    case "Revert":
+                        switch (lastDirection) {
+                            case "E":
+                                horizontalIndex = horizontalIndex - setX;
+                                break;
+                            case "W":
+                                horizontalIndex = horizontalIndex + setX;
+                                break;
+                            case "N":
+                                verticalIndex = verticalIndex + setY;
+                                break;
+                            case "S":
+                                verticalIndex = verticalIndex - setY;
+                                break;
+
+                        }
+                        break;
+                }
+                System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
 }
